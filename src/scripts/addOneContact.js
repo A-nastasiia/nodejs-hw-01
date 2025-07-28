0f8faa92e9d2706ad5d38b6647078adc350bca8c
Anastasiia <anastasiianikolaienko2004@gmail.com>
2025-07-28 13:22:22 +0200

    0

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -6,11 +6,11 @@ export const addOneContact = async () => {
     try{
         const existingContacts = await readContacts() || [];
         const newContacts = createFakeContact();
-        const updateContacs = [...existingContacts, newContacts];
-        await writeContacts(updateContacs);
+        const updateContacts = [...existingContacts, newContacts];
+        await writeContacts(updateContacts);
         console.log('Successfully added one contact', newContacts);
     } catch(err){
-        console.error('Error of adding contact', err.massage);
+        console.error('Error of adding contact', err.message);
     }
 };
 
